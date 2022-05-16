9344d7a594791e88c8278d414e9255d72eccf076
Emiliano <e.acevedo.felip@gmail.com>
2022-05-16 15:42:32 -0400

    ADD: Countries now displayed by continents or lang
    The countries are displayed by continents or by languages. The users can
    control how they want to watch its. If they select Continent, all
    continents are display with all the respective countries. The same way
    goes by languages.

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,35 +1,18 @@
-import { useQuery, gql } from "@apollo/client";
-
 import styles from './Results.module.css'
 
 import Card from "../Card/Card";
 
-const COUNTRIES_DATA_QUERY = gql`
-  {
-    countries {
-      name,
-      emoji,
-      code,
-      continent {name},
-      languages {name},
-      capital,
-      currency,
-      phone
-    }
-  }
-`;
+export default function Results(props) {
+  const continent = props.data[0]
+  const countries = props.data[1]
 
-export default function Results() {
-  const { data, loading, error } = useQuery(COUNTRIES_DATA_QUERY);
-
-  if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>
-
-  else return (
+  // console.log(continent)
+  
+  return (
     <>
-      <h3>Spanish</h3>
+      <h3>{continent}</h3>
       <div className={styles.cardsContainer}>
-        {data.countries.map(country => <Card key={country.name} data={country}/>)}
+        {countries.map(country => <Card key={country.name} data={country}/>)}
       </div>
     </>
   )
